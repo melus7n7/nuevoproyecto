0e280bddf3027cda311230f82c17f51c708d39dd
melus7n7 <sulem477@gmail.com>
2024-03-09 22:01:47 -0600

    AppSinHilos

---
+++ dsr/AppSinHilos/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace AppSinHilos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Sulem Martínez Aguilar Desarrollo de sistemas en red");
+        Console.WriteLine("El main thread comienza aquí");
+
+        Program.DoTrabajoPesado();
+
+        Program.DoAlgo();
+
+        Console.WriteLine("El main thread termina aquí");
+        Console.WriteLine("Presione cualquier tecla para terminar");
+        Console.ReadKey(true);
+    }
+    public static void DoTrabajoPesado(){
+        Console.WriteLine("DoTrabajoPesado: Estoy levantando un camión!!");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: Cansado! Necesito una siesta de 3 seg");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 1....");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 2....");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 3....");
+        Console.WriteLine("DoTrabajoPesado: Ya desperté");
+    }
+    public static void DoAlgo(){
+        Console.WriteLine("DoAlgo: Oye! Estoy haciendo algo aquí!");
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write($"{i} ");         
+        }
+        Console.WriteLine();
+        Console.WriteLine("DoAlgo: Ya terminé!");
+    }
+}
