70d36ae94a696e6e895b3dcbc5820b5f1b82d840
melus7n7 <sulem477@gmail.com>
2024-03-11 21:39:13 -0600

    AppConHilos2 Y AppConHilos3

---
+++ dsr/AppConHilos2/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace AppConHilos2;
+
+//Sulem Martínez Aguilar
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Thread currentThread = Thread.CurrentThread;
+        currentThread.Name = "Hilo principal";
+        currentThread.Priority = ThreadPriority.Lowest;
+        currentThread.IsBackground = false;
+        Console.WriteLine("Thread Id: {0}", currentThread.ManagedThreadId);
+        Console.WriteLine("Thread Name: {0}", currentThread.Name);
+        Console.WriteLine("Thread State: {0}", currentThread.ThreadState);
+        Console.WriteLine("Es un thread background: {0}", currentThread.IsBackground);
+        Console.WriteLine("Priority: {0}", currentThread.Priority);
+        Console.WriteLine("Culture: {0}", currentThread.CurrentCulture.Name);
+        Console.WriteLine("UI Culture: {0}", currentThread.CurrentUICulture.Name);
+        Console.WriteLine();
+        
+        Thread workerThread = new Thread(new ParameterizedThreadStart(Print));
+        workerThread.Name = "Hilo de print";
+        CancellationTokenSource cts = new  CancellationTokenSource();
+        workerThread.Start(cts.Token);
+
+        for(int i=0; i<10; i++)
+        {
+            Console.WriteLine($"Principal thread: {i}");
+            Thread.Sleep(200);
+        }
+        if(workerThread.IsAlive){
+            cts.Cancel();
+        }
+        
+    }
+    static void Print(object? obj)
+    {
+        if(obj == null)
+            return;
+
+        CancellationToken token = (CancellationToken) obj;
+
+        Thread currentThread = Thread.CurrentThread;
+        currentThread.Priority = ThreadPriority.Highest;
+        currentThread.IsBackground = false;
+
+        Console.WriteLine("Thread Id: {0}", currentThread.ManagedThreadId);
+        Console.WriteLine("Thread Name: {0}", currentThread.Name);
+        Console.WriteLine("Thread State: {0}", currentThread.ThreadState);
+        Console.WriteLine("Es un thread background: {0}", currentThread.IsBackground);
+        Console.WriteLine("Priority: {0}", currentThread.Priority);
+        Console.WriteLine("Culture: {0}", currentThread.CurrentCulture.Name);
+        Console.WriteLine("UI Culture: {0}", currentThread.CurrentUICulture.Name);
+        Console.WriteLine();
+
+        for(int i=11; i<20; i++)
+        {
+            if(token.IsCancellationRequested)
+            {
+                Console.WriteLine("En la iteración {0}, la cancelación ha sido solicitada...", i);
+                break;
+            }
+            Console.WriteLine($"Print thread: {i}");
+            Thread.Sleep(1000);
+        }
+        
+    }
+}
---
+++ dsr/AppConHilos3/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace AppConHilos3;
+
+public class Persona
+{
+    public string Nombre { get; set;}
+    public int Edad { get; set; }
+    public string Sexo { get; set; }
+
+    public Persona(string nombre, int edad, string sexo)
+    {
+        this.Nombre = nombre;
+        this.Edad = edad;
+        this.Sexo = sexo;
+    }
+
+    static void TareaDeFondoConParametro(Object? stateInfo)
+    {
+        if(stateInfo == null)
+            return;
+
+        Thread currentThread = Thread.CurrentThread;
+        Console.WriteLine("Es un thread background: {0}", currentThread.IsBackground);
+
+        Persona data = (Persona)stateInfo;
+        Console.WriteLine($"Hilo 2: Hola {data.Nombre} desde ThreadPool, tu edad es {data.Edad}");
+        Thread.Sleep(500);
+    }
+
+    static void TareaDeFondo(Object? stateInfo)
+    {
+        Console.WriteLine($"Hilo 1: Holaa soy un hilo sin parámetros del ThreadPool");
+        Thread.Sleep(1500);
+    }
+
+    static void Main(string[] args)
+    {
+        int workers, ports;
+        ThreadPool.GetMaxThreads(out workers, out ports);
+        Console.WriteLine($"Máximos hilos de trabajo: {workers}");
+        Console.WriteLine($"Máximos puertos para hilos: {ports}");
+
+        ThreadPool.GetMinThreads(out workers, out ports);
+        Console.WriteLine($"Mínimos hilos de trabajo: {workers}");
+        Console.WriteLine($"Mínimos puertos para hilos: {ports}");
+
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles: {workers}");
+        Console.WriteLine($"Puertos para hilos disponibles: {ports}");
+
+        int processCount = Environment.ProcessorCount;
+        Console.WriteLine($"No. de procesadores disponibles en el sistema: {processCount}");
+        Console.WriteLine($"--------------------------------");
+
+        ThreadPool.QueueUserWorkItem(TareaDeFondo);
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles después del hilo 1: {workers}");
+
+        Persona p = new Persona("Sulem Martínez", 20, "Mujer");
+        ThreadPool.QueueUserWorkItem(TareaDeFondoConParametro, p);
+        
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles después del hilo 2: {workers}");
+
+        Thread.Sleep(2000);
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles al final: {workers}");
+
+        Console.ReadKey();
+    }
+
+}
+
